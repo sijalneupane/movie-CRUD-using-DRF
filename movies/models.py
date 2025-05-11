5d8aa62c2af0f09c83749666d47440bf4c2ba056
sijalneupane <sijalneupane5@gmail.com>
2025-05-11 11:47:02 +0545

    second time commit

---
+++ movies/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class MovieData(models.Model):
+    name=models.CharField(max_length=200)
+    duration=models.FloatField()
+    rating=models.FloatField(default=0.0)
+    typ=models.CharField(max_length=200,default='action')
+    image=models.ImageField(upload_to='Images',default='Images/None/Noimg.jpg')
+
+    def __str__(self):
+        return self.name
